df57a43e2e03f01e329ba925ffa9c3ff78f0de80
Prashanth <yadavcec@gmail.com>
2026-05-26 13:43:34 +0530

    Conditional Statement Assignment

---
+++ conditionalStatements_assignment.spec.js
@@ -0,0 +1,39 @@
+function launchBrowser(){
+    let browserName = "Firefox"
+
+    if(browserName == "Chrome"){
+        console.log("Chrome browser launched")
+    }else if(browserName == "Firefox"){
+        console.log("Firefox borwser launched")
+    }else {
+        console.log("Select proper browser")
+    }
+}
+//---------------
+
+function  runTest(){
+    let testType = "Regression"
+
+    switch(testType){
+
+        case "Integration": 
+            console.log("Integration testing required/in-progress")
+            break;
+
+        case "Sanity":
+            console.log("Sanity testing required/in-progress")
+            break;
+    
+        case "Regression":
+            console.log("Regression testing required/ in-progress")
+            break;
+
+        default:
+            console.log("Somke testing required/ in-progress")
+            break;
+
+    }
+}
+
+launchBrowser()
+runTest() 
