eb51edb686297f6d02f031caec916d5876b8111e
Prashanth <yadavcec@gmail.com>
2026-05-26 11:28:18 +0530

    Add files via upload
    Classroom Assignment: Number Type 01

---
+++ numberType_assignment1.spec.js
@@ -0,0 +1,12 @@
+
+
+let number = -3000
+if(number>0){
+    console.log(number.valueOf)
+    console.log("its a positive number")
+}else if(number <0){
+    console.log("its a negative number")
+}else {
+    console.log("its a Zero")
+}
+
