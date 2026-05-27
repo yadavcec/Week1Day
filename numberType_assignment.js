9144aca4334c1a7be71a346c9a583abe664ed48c
Prashanth <yadavcec@gmail.com>
2026-05-27 12:35:48 +0530

    Update and renamed the file

---
+++ numberType_assignment.js
@@ -0,0 +1,14 @@
+
+function numberType(){
+    let number = -3
+    if(number > 0){
+        console.log(number.valueOf())
+        console.log("It's a positive number")
+    }else if(number < 0){
+        console.log("It's a negative number")
+    }else {
+        console.log("It's a Zero")
+    }
+}
+
+numberType()
